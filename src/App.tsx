e5c5aeb8e65c43a4d2c0ae62c83c7a10c4f13181
turkaytunc <turkaytunc00@gmail.com>
2021-02-07 11:07:46 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <Header projectName={"crasss"} />
+      <Header projectName={"cra-t"} />
     </div>
   );
 }
